82a33459041023c2fe7a9b30e716936331eee482
jaemyeong-hwnag <woaudsla@gmail.com>
2023-04-03 20:08:31 +0900

    HelloServlet 생성 및 http 디버그로그 추가

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String userName = request.getParameter("username");
+        System.out.println("username = " + userName);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(userName);
+    }
+}
